770b2ed324f2f06174c7226b309673a8f0722de1
vchristopoulos <v.christopoulos@divbyzero.gr>
2022-10-27 10:35:09 +0300

    created colors table and test fixes at null-ls

---
+++ lua/core/color_themes.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+M.mellow = {
+	bg = "#161617",
+	fg = "#c9c7cd",
+	black = "#27272a",
+	red = "#f5a191",
+	green = "#90b99f",
+	yellow = "#e6b99d",
+	blue = "#aca1cf",
+	magenta = "#e29eca",
+	cyan = "#ea83a5",
+	white = "#c1c0d4",
+	orange = "#FFC590",
+	darkblue = "#3C6BBA",
+}
+
+M.default_lualine = {
+	bg = "#202328",
+	fg = "#bbc2cf",
+	yellow = "#ECBE7B",
+	cyan = "#008080",
+	darkblue = "#081633",
+	green = "#98be65",
+	orange = "#FF8800",
+	violet = "#a9a1e1",
+	magenta = "#c678dd",
+	blue = "#51afef",
+	red = "#ec5f67",
+}
+
+return M
--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -16,17 +16,17 @@ require("null-ls").setup({
 		diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 	},
 	on_attach = function(client, bufnr)
-		-- if client.name == "tsserver" or client.name == "rust_analyzer" or client.name == "pyright" then
-		--   client.resolved_capabilities.document_formatting = false
-		-- end
+		if client.name == "tsserver" or client.name == "rust_analyzer" or client.name == "pyright" then
+			client.resolved_capabilities.document_formatting = false
+		end
 
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
 				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-					-- vim.lsp.buf.formatting_sync()
+					-- vim.lsp.buf.format({ bufnr = bufnr })
+					vim.lsp.buf.formatting_sync()
 				end,
 			})
 		end
