3d7966e5445e449cfd9cd33f074b93b796367495
Vasilis Chrtistopoulos <vaschristopoulos@gmail.com>
2023-04-06 23:42:00 +0300

    chore: formatting

--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -13,19 +13,19 @@ require("null-ls").setup({
 		formatting.phpcsfixer,
 		formatting.prettier,
 		formatting.stylua,
-        formatting.phpcbf,
+		formatting.phpcbf,
 		diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 	},
 	on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
