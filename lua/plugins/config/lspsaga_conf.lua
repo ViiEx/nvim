73da911862bdc15540ca11437710f8c431ec240e
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2023-04-20 12:37:46 +0300

    chore: Since I update to nvim 0.9 we can have some extra lsp saga configs

--- lua/plugins/config/lspsaga_conf.lua
+++ lua/plugins/config/lspsaga_conf.lua
@@ -1,15 +1,31 @@
 local status_ok, lspsaga = pcall(require, "lspsaga")
 
 if not status_ok then
-    return
+	return
 end
 
 lspsaga.setup({
-    lightbulb = {
-        enable = false,
-        enable_in_insert = false,
-        sign = false,
-        sign_priority = 40,
-        virtual_text = false,
-    },
+	lightbulb = {
+		enable = false,
+		enable_in_insert = false,
+		sign = false,
+		sign_priority = 40,
+		virtual_text = false,
+	},
+	ui = {
+		-- This option only works in Neovim 0.9
+		title = true,
+		-- Border type can be single, double, rounded, solid, shadow.
+		border = "single",
+		winblend = 0,
+		expand = "",
+		collapse = "",
+		code_action = "💡",
+		incoming = " ",
+		outgoing = " ",
+		hover = " ",
+		kind = {
+			["Folder"] = { " ", "@comment" },
+		},
+	},
 })
