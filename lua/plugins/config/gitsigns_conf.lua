d16725e9ec1877179f84bab03cc14d3e763d7db6
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2023-04-26 10:16:52 +0300

    fix: fixed git sign colors

--- lua/plugins/config/gitsigns_conf.lua
+++ lua/plugins/config/gitsigns_conf.lua
@@ -6,14 +6,32 @@ if not present then
 end
 
 local utils = require("core.utils")
+local HL = utils.HL
+
+HL("DiffAdd", { foreground = "#79DCAA" })
+HL("DiffChange", { foreground = "#C397D8" })
+HL("DiffDelete", { foreground = "#F87070" })
+HL("DiffText", { foreground = "#7AB0DF" })
 
 local options = {
 	signs = {
-		add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
-		change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-		delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
-		topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-		changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
+		add = { texthl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		change = {
+			hl = "DiffChange",
+			text = "│",
+		},
+		delete = {
+			hl = "DiffDelete",
+			text = "",
+		},
+		topdelete = {
+			hl = "DiffDelete",
+			text = "‾",
+		},
+		changedelete = {
+			hl = "DiffChangeDelete",
+			text = "~",
+		},
 		untracked = { hl = "DiffUntracked", text = "┆", numhl = "GitSignsUntrackedNr" },
 	},
 	current_line_blame = false,
