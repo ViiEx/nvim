c50733f1b855baebf197925302df58694c75cfae
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2023-10-20 16:44:29 +0300

    fix: migrade to v3 for indent_blackline

--- lua/plugins/config/blankline.lua
+++ lua/plugins/config/blankline.lua
@@ -1,27 +1,97 @@
-local present, blankline = pcall(require, "indent_blankline")
+local status_ok, blankline = pcall(require, "ibl")
 
-if not present then
+if not status_ok then
 	return
 end
 
-local options = {
-	indentLine_enabled = 1,
-	filetype_exclude = {
-		"help",
-		"terminal",
-		"lazy",
-		"lspinfo",
-		"TelescopePrompt",
-		"TelescopeResults",
-		"mason",
-        "alpha",
-		"",
-	},
-	buftype_exclude = { "terminal" },
-	show_trailing_blankline_indent = false,
-	show_first_indent_level = false,
-	show_current_context = true,
-	show_current_context_start = false,
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
 }
 
-blankline.setup(options)
+local hooks = require("ibl.hooks")
+
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+require("ibl").setup({
+	enabled = true,
+	debounce = 200,
+	viewport_buffer = {
+		min = 30,
+		max = 500,
+	},
+	indent = {
+		char = "▎",
+		tab_char = nil,
+		highlight = highlight,
+		smart_indent_cap = true,
+		priority = 1,
+	},
+	whitespace = {
+		highlight = "IblWhitespace",
+		remove_blankline_trail = true,
+	},
+	scope = {
+		enabled = true,
+		char = nil,
+		show_start = true,
+		show_end = true,
+		show_exact_scope = false,
+		injected_languages = true,
+		highlight = "IblScope",
+		priority = 1024,
+		include = {
+			node_type = {},
+		},
+		exclude = {
+			language = {},
+			node_type = {
+				["*"] = {
+					"source_file",
+					"program",
+				},
+				lua = {
+					"chunk",
+				},
+				python = {
+					"module",
+				},
+			},
+		},
+	},
+	exclude = {
+		filetypes = {
+			"lspinfo",
+			"packer",
+			"checkhealth",
+			"help",
+			"man",
+			"gitcommit",
+			"TelescopePrompt",
+			"TelescopeResults",
+			"mason",
+			"lazy",
+			"terminal",
+			"alpha",
+		},
+		buftypes = {
+			"terminal",
+			"nofile",
+			"quickfix",
+			"prompt",
+		},
+	},
+})
