80dc4daa97ef334dddb2ddb64c28ecf00719e093
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2023-11-17 15:42:44 +0200

    fix: broken icons for lsp

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,39 +1,39 @@
 -- Taken from NvChad ui lsp
 
 local function lspSymbol(name, icon)
-  local hl = "DiagnosticSign" .. name
-  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+	local hl = "DiagnosticSign" .. name
+	vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
 end
 
-lspSymbol("Error", "")
-lspSymbol("Info", "")
-lspSymbol("Hint", "")
+lspSymbol("Error", "󰅙")
+lspSymbol("Info", "󰋼")
+lspSymbol("Hint", "󰌵")
 lspSymbol("Warn", "")
 
-vim.diagnostic.config {
-  virtual_text = {
-    prefix = "",
-  },
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-}
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "",
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+})
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "single",
+	border = "single",
 })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "single",
-  focusable = false,
-  relative = "cursor",
+	border = "single",
+	focusable = false,
+	relative = "cursor",
 })
 
 -- Borders for LspInfo winodw
-local win = require "lspconfig.ui.windows"
+local win = require("lspconfig.ui.windows")
 local _default_opts = win.default_opts
 
 win.default_opts = function(options)
-  local opts = _default_opts(options)
-  opts.border = "single"
-  return opts
+	local opts = _default_opts(options)
+	opts.border = "single"
+	return opts
 end
