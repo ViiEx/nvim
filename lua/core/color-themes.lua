d7e62998d04f28f9be2994deb69644f5df09e327
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2023-11-22 17:14:08 +0200

    feat: table for themes

---
+++ lua/core/color-themes.lua
@@ -0,0 +1,9 @@
+local themes = {
+	DEFAULT = "catppuccin-latte",
+	CATPPUCCIN_LATTE = "catppuccin-latte",
+	CATPPUCCIN_FRAPPE = "catppuccin-frappe",
+	CATPPUCCIN_MACCHIATO = "catppuccin-macchiato",
+	CATPPUCCIN_MOCHA = "catppuccin-mocha",
+}
+
+return themes
